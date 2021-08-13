da22a6191923197c0bea4333203b564f100f8397
cfeteira <cfeteira@gmail.com>
2021-08-13 21:23:37 +0100

    commit com o test.sh

--- ATUALIZAR.bat
+++ ATUALIZAR.bat
@@ -1,6 +1,4 @@
 python test.py
-git config --global user.email "cfeteira@gmail.com"
-git config --global user.name "cfeteira"
 git init
 git remote add origin https://github.com/websistemath/torredecontrolo.git
 git add *
