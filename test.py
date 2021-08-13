b2f928d62fedbe2bda4026fc468c195ac582a174
websitemath <farruchocenas@gmail.com>
2021-08-13 20:24:54 +0100

    commit com o test.sh

--- test.py
+++ test.py
@@ -159,7 +159,7 @@ def write2html(line,barra_perc):
         <html lang="pt-pt">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <head>
-            <link rel="stylesheet" href="samplestyle.css">
+            <link rel="stylesheet" href="../samplestyle.css">
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
