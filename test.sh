3bc8920fb54cc669870ba572c6643e2dbd185566
websitemath <farruchocenas@gmail.com>
2021-08-13 14:31:52 +0100

    commit
    abriu se o test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 python test.py
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=999999999"
+git init
+git remote add origin https://github.com/websistemath/torredecontrolo.git
 git add *
 git status
 git commit -m "commit" -m "abriu se o test.sh"
