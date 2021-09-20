18fda236e88da6246b70cf302cb9cf5c39ced56c
cfeteira <cfeteira@gmail.com>
2021-09-20 19:20:09 +0100

    commit com o test.sh

--- ATUALIZAR.bat
+++ ATUALIZAR.bat
@@ -1,4 +1,4 @@
-cd C:\Users\laran\OneDrive\Ambiente de Trabalho\2021-2022\7ano\GAMIFICAÇÃO\torredecontrolo
+cd "C:\Users\laran\OneDrive\Ambiente de Trabalho\2021-2022\7ano\GAMIFICAÇÃO\torredecontrolo"
 python test.py
 git init
 git remote add origin https://github.com/websistemath/torredecontrolo.git
