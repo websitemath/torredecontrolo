c12db8cf4ae605cfb1b4f8c08eb8ad5b056121ac
websitemath <88887415+websitemath@users.noreply.github.com>
2023-09-06 18:28:23 +0100

    updated titulo

--- test.py
+++ test.py
@@ -313,7 +313,7 @@ def writeindex(dadosOrganizadosPorPontos,top):
         </script>
     </head>
     <body>
-        <h1 id="maintitle">Torre de Controlo</h1>
+        <h1 id="maintitle">Ranking do Desafio Matemático Mensal</h1>
         <h1>Coloca os teus dados no formato: ANOTURMA_NUMERO</h1>
         <input type="text" id="turmanum" placeholder="Exemplo: 6A_11"><br>
         <button id="botao" onclick="botao()">Carrega aqui</button>
