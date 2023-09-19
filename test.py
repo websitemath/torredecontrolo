670b7823972e17d662cd48879ffedbf2fd99d9b4
websitemath <88887415+websitemath@users.noreply.github.com>
2023-09-19 23:39:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -122,7 +122,7 @@ def write2html(line,barra_perc):
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>""" + str(line[3]) + """</title>
+            <title>""" + str(line[1]) + "_" + str(line[2]) + """</title>
         </head>
         <body>
             <h1 id="nomealuno"> """ + 'Estat&iacute;sticas do aluno' + """</h1>
